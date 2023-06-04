4b392ba8cb4caa4928c5fd8460cc4647dc92a2e1
tuanpa03 <phamanhtuan2833@gmail.com>
2023-06-04 12:23:47 +0700

    feat: crud category refs: AW-4 AW-5 AW-6 AW-7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardPageComponent } from './pages/admin/dashboard-page/dashboard-page.component';
+import { CategoriesPageComponent } from './pages/admin/categories-page/categories-page.component';
+import { CategoryAddComponent } from './pages/admin/category-add/category-add.component';
+import { CategoryUpdateComponent } from './pages/admin/category-update/category-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BaseLayoutComponent },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardPageComponent },
+      { path: 'categories', component: CategoriesPageComponent },
+      { path: 'categories/add', component: CategoryAddComponent },
+      { path: 'categories/:id/update', component: CategoryUpdateComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
